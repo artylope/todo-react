998fbcf390951e224e477db04092c2111e917359
artylope <angyixin@gmail.com>
2019-08-29 13:30:06 +0800

    list tasks finish and not finish

--- script.jsx
+++ script.jsx
@@ -4,7 +4,21 @@ class List extends React.Component {
 
     this.state = {
       task:"",
-      list : []
+      list : [
+          {
+            "task": "Eat laksa",
+            "done": "done",
+            "created_at": "",
+            "updated_at": ""
+          },
+          {
+            "task": "Go swimming",
+            "done": "done",
+            "created_at": "",
+            "updated_at": ""
+          },
+
+      ]
     }
   }
 
@@ -37,19 +51,27 @@ class List extends React.Component {
   render() {
       // render the list with a map() here
       let listItems = this.state.list.map( (listTtem, index) => {
+
+          if(listTtem.done === "false"){
+              return ( <li key={index} > Not FINISH {listTtem.task}</li> );
+          } else if(listTtem.done === "done"){
+              return ( <li key={index} > FINISH {listTtem.task}</li> );
+          }
+
           return ( <li key={index} >{listTtem.task}, {listTtem.done}</li> );
         });
       console.log("rendering");
       return (
-        <div className="list">
-          <input onChange={(event)=>{this.changeHandler(event)}} value={this.state.task}/>
-          <button onClick={(event)=>{this.addItem(event)}}>add item</button>
-
-
-          <ul>
-            {listItems}
-
-          </ul>
+         <div class="container">
+            <div class="add-item">
+                <input onChange={(event)=>{this.changeHandler(event)}} value={this.state.task}/>
+                <button onClick={(event)=>{this.addItem(event)}}>Add</button>
+            </div>
+            <div className="list">
+              <ul>
+                {listItems}
+              </ul>
+            </div>
         </div>
       );
   }
