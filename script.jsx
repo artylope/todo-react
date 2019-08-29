110efd7d5040b5db513e6e6cb5baabcea1294a3d
artylope <angyixin@gmail.com>
2019-08-29 13:59:32 +0800

    display list with check boxes

--- script.jsx
+++ script.jsx
@@ -1,3 +1,4 @@
+
 class List extends React.Component {
   constructor(){
     super()
@@ -8,14 +9,14 @@ class List extends React.Component {
           {
             "task": "Eat laksa",
             "done": "done",
-            "created_at": "",
-            "updated_at": ""
+            "created_at": "August 29th 2019, 1:40:58 pm",
+            "updated_at": "August 29th 2019, 1:40:58 pm"
           },
           {
             "task": "Go swimming",
             "done": "done",
-            "created_at": "",
-            "updated_at": ""
+            "created_at": "August 29th 2019, 1:40:58 pm",
+            "updated_at": "August 29th 2019, 1:40:58 pm"
           },
 
       ]
@@ -25,23 +26,23 @@ class List extends React.Component {
   addItem(event){
     // debugger;
     let newTask = this.state.task;
-    console.log('inside add item button. new task: ' + newTask );
+    // console.log('inside add item button. new task: ' + newTask );
 
     this.state.list.push({
        "task": newTask,
        "done": "false",
-       "created_at": "",
-       "updated_at": ""
+       "created_at": moment().format('MMMM Do YYYY, h:mm:ss a'),
+       "updated_at": moment().format('MMMM Do YYYY, h:mm:ss a')
     })
 
     this.setState({ list: this.state.list });
 
-    console.log(this.state.list);
+    // console.log(this.state.list);
   }
 
   changeHandler(){
     // debugger;
-    console.log('inside changeHandler. Word: ' + event.target.value);
+    // console.log('inside changeHandler. Word: ' + event.target.value);
 
     this.setState({
         task: event.target.value
@@ -53,14 +54,23 @@ class List extends React.Component {
       let listItems = this.state.list.map( (listTtem, index) => {
 
           if(listTtem.done === "false"){
-              return ( <li key={index} > Not FINISH {listTtem.task}</li> );
+              return (
+                  <div className="list-item" key={index} >
+                      <i class='bx bx-checkbox'></i>
+                        {listTtem.task}
+                </div>
+                    );
           } else if(listTtem.done === "done"){
-              return ( <li key={index} > FINISH {listTtem.task}</li> );
+              return (
+                  <div className="list-item completed" key={index} >
+                      <i class='bx bx-checkbox-checked' ></i>
+                        {listTtem.task}
+                    </div>
+                );
           }
 
-          return ( <li key={index} >{listTtem.task}, {listTtem.done}</li> );
         });
-      console.log("rendering");
+      // console.log("rendering");
       return (
          <div class="container">
             <div class="add-item">
@@ -68,9 +78,7 @@ class List extends React.Component {
                 <button onClick={(event)=>{this.addItem(event)}}>Add</button>
             </div>
             <div className="list">
-              <ul>
                 {listItems}
-              </ul>
             </div>
         </div>
       );
