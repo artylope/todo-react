1350641946a2f2293b710f2670f392176cef6fc9
artylope <angyixin@gmail.com>
2019-09-02 06:17:45 +0800

    remove the unnecessary ids

--- todosData.js
+++ todosData.js
@@ -1,34 +1,29 @@
 const todosData = [
     {
-        id: 1,
         "task": "Eat Chicken Rice",
         "done": true,
         "created_at": "2019-08-11T16:06:11+08:00",
         "updated_at": "2019-08-11T16:06:11+08:00"
     },
     {
-        id: 2,
         "task": "Go swimming",
         "done": true,
         "created_at": "2019-08-22T16:08:11+08:00",
         "updated_at": "2019-08-22T16:08:11+08:00"
     },
     {
-        id: 3,
         "task": "Play Mario Kart",
         "done": false,
         "created_at": "2019-08-25T16:06:11+08:00",
         "updated_at": "2019-08-25T16:06:11+08:00"
     },
     {
-        id: 4,
         "task": "Sing Karaoke",
         "done": true,
         "created_at": "2019-08-26T16:08:11+08:00",
         "updated_at": "2019-08-26T16:08:11+08:00"
     },
     {
-        id: 5,
         "task": "Do homework",
         "done": false,
         "created_at": "2019-09-01T06:55:11+08:00",
